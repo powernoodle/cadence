b6e287e8a523225a03b3f30ca2d095126466524d
Kris Braun <kris.braun@letscollide.io>
2023-05-03 14:40:57 -0400

    nx

---
+++ cf-build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+NPM_OPTS=
+
+npm -g i pnpm
+pnpm i --store=node_modules/.pnpm-store
+
+case $1 in
+  web)
+    pnpm nx run web:build
+    ;;
+
+  *)
+    echo -n "Usage: $0 {web}"
+    ;;
+esac

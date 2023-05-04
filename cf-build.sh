50d0061a9ada8c747799f404193990c98359869f
Kris Braun <kris.braun@letscollide.io>
2023-05-04 13:38:24 -0400

    Re-add optional dependancies

--- cf-build.sh
+++ cf-build.sh
@@ -6,7 +6,7 @@ echo === Installing pnpm ===
 npm -g i pnpm || exit 1
 
 echo === Installing pnpm packages ===
-pnpm i --store=node_modules/.pnpm-store --no-optional || exit 1
+pnpm i --store=node_modules/.pnpm-store || exit 1
 
 case $1 in
   web)

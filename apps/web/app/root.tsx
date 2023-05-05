8989b8f5f58affa7e1eacc4f770b4bb963cabefd
Kris Braun <kris.braun@letscollide.io>
2023-05-04 22:32:30 -0400

    Supabase client to root

--- apps/web/app/root.tsx
+++ apps/web/app/root.tsx
@@ -1,4 +1,6 @@
 import type { LinksFunction } from "@remix-run/cloudflare";
+import type { LoaderArgs } from "@remix-run/cloudflare"; // change this import to whatever runtime you are using
+
 import { cssBundleHref } from "@remix-run/css-bundle";
 import {
   Links,
@@ -8,12 +10,31 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+import { useEffect, useState } from "react";
+import { useLoaderData } from "@remix-run/react";
+import { json } from "@remix-run/cloudflare"; // change this import to whatever runtime you are using
+import { PostgrestError, createClient } from "@supabase/supabase-js";
+
+export const loader = ({ context }: LoaderArgs) => {
+  const env = {
+    SUPABASE_URL: context.SUPABASE_URL!,
+    SUPABASE_ANON_KEY: context.SUPABASE_ANON_KEY!,
+  };
+
+  return json({ env });
+};
 
 export const links: LinksFunction = () => [
   ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
 ];
 
 export default function App() {
+  const { env } = useLoaderData();
+
+  const [supabase] = useState(() =>
+    createClient(env.SUPABASE_URL, env.SUPABASE_ANON_KEY)
+  );
+
   return (
     <html lang="en">
       <head>
@@ -23,7 +44,7 @@ export default function App() {
         <Links />
       </head>
       <body>
-        <Outlet />
+        <Outlet context={{ supabase }} />
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
--- apps/web/app/routes/_index.tsx
+++ apps/web/app/routes/_index.tsx
@@ -1,32 +1,10 @@
 import type { V2_MetaFunction } from "@remix-run/cloudflare";
-import type { LoaderArgs } from "@remix-run/cloudflare"; // change this import to whatever runtime you are using
-
-import { useEffect, useState } from "react";
-import { useLoaderData } from "@remix-run/react";
-import { json } from "@remix-run/cloudflare"; // change this import to whatever runtime you are using
-// import { createServerClient } from '@supabase/auth-helpers-remix'
-import { PostgrestError, createClient } from "@supabase/supabase-js";
 
 export const meta: V2_MetaFunction = () => {
   return [{ title: "New Remix App" }];
 };
 
-export const loader = ({ context }: LoaderArgs) => {
-  const env = {
-    SUPABASE_URL: context.SUPABASE_URL!,
-    SUPABASE_ANON_KEY: context.SUPABASE_ANON_KEY!,
-  };
-
-  return json({ env });
-};
-
 export default function Index() {
-  // const { env } = useLoaderData();
-  //
-  // const [supabase] = useState(() =>
-  //   createClient(env.SUPABASE_URL, env.SUPABASE_ANON_KEY)
-  // );
-
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
       <h1>Welcome to Remix</h1>

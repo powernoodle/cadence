6b98e1bcbca47509617b27294e9ce3e427ee7a89
Kris Braun <kris.braun@letscollide.io>
2023-05-22 21:08:38 -0400

    Add Sentry to web

---
+++ apps/web/functions/_middleware.ts
@@ -0,0 +1,5 @@
+import sentryPlugin from "@cloudflare/pages-plugin-sentry";
+
+export const onRequest: PagesFunction = sentryPlugin({
+  dsn: "https://5d95ce1a49ab4eb1956c0d5a15f55960:11e336af2ac04c3a931a5cd33230d662@o4505134083997696.ingest.sentry.io/4505134085832704",
+});

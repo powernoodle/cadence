0e2db047263cc7c5cfa06a00f3e5d33fe8d1fe71
Kris Braun <kris.braun@letscollide.io>
2023-05-22 20:57:39 -0400

    Worker Sentry logging

--- apps/sync/src/index.ts
+++ apps/sync/src/index.ts
@@ -1,12 +1,4 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `wrangler dev src/index.ts` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `wrangler publish src/index.ts --name my-worker` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { Toucan } from "toucan-js";
 
 import add from "date-fns/add";
 import sub from "date-fns/sub";
@@ -20,6 +12,7 @@ export type SyncRequest = {
 export interface Env {
   readonly ENV?: string;
 
+  readonly SENTRY_DSN: string;
   readonly SUPABASE_URL: string;
   readonly SUPABASE_KEY: string;
   readonly GOOGLE_CLIENT_ID: string;
@@ -31,16 +24,34 @@ export interface Env {
 }
 
 export default {
-  async fetch(req: Request, env: Env): Promise<Response> {
-    if (env.ENV !== "dev") {
-      return new Response("Denied");
+  async fetch(
+    request: Request,
+    env: Env,
+    context: ExecutionContext
+  ): Promise<Response> {
+    const sentry = new Toucan({
+      dsn: env.SENTRY_DSN,
+      context,
+      request,
+    });
+
+    try {
+      if (env.ENV !== "dev") {
+        return new Response("Denied");
+      }
+      if (request.method !== "POST") return new Response("Ignored");
+      const body = await request.json();
+      if (!body || typeof (body as any).accountId !== "number")
+        return new Response("Missing accountId");
+      await process(env, body as SyncRequest);
+      return new Response("Success");
+    } catch (e) {
+      sentry.captureException(e);
+
+      return new Response("Worker error", {
+        status: 500,
+      });
     }
-    if (req.method !== "POST") return new Response("Ignored");
-    const body = await req.json();
-    if (!body || typeof (body as any).accountId !== "number")
-      return new Response("Missing accountId");
-    await process(env, body as SyncRequest);
-    return new Response("Success");
   },
 
   // Invoked when the Worker receives a batch of messages.
@@ -67,7 +78,7 @@ export default {
   },
 };
 
-async function process(env: Env, req: SyncRequest) {
+async function process(env: Env, request: SyncRequest) {
   const store = await CalendarStore.Create(
     env.SUPABASE_URL!,
     env.SUPABASE_KEY!,
@@ -75,7 +86,7 @@ async function process(env: Env, req: SyncRequest) {
     env.GOOGLE_OAUTH_SECRET!,
     env.MICROSOFT_CLIENT_ID!,
     env.MICROSOFT_OAUTH_SECRET!,
-    req.accountId
+    request.accountId
   );
   await store.syncEvents(
     sub(new Date(), { days: 60 }),

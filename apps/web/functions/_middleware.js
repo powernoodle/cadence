7e1298b462f88a1774c31f250092776eb4268429
Kris Braun <kris.braun@letscollide.io>
2023-05-22 23:13:49 -0400

    Remove Sentry

--- apps/web/functions/_middleware.js
+++
@@ -1,5 +0,0 @@
-import sentryPlugin from "@cloudflare/pages-plugin-sentry";
-
-export const onRequest = sentryPlugin({
-  dsn: "https://5d95ce1a49ab4eb1956c0d5a15f55960:11e336af2ac04c3a931a5cd33230d662@o4505134083997696.ingest.sentry.io/4505134085832704",
-});

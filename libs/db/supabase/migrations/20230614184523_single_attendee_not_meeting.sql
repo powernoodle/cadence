e9e0fc89758e761cbb214c6979a159405b0f8983
Kris Braun <kris.braun@letscollide.io>
2023-06-14 14:50:04 -0400

    Meetings with a single attendee aren't meetings

---
+++ libs/db/supabase/migrations/20230614184523_single_attendee_not_meeting.sql
@@ -0,0 +1,29 @@
+DROP FUNCTION IF EXISTS "public"."update_attendees" (this_event_id bigint, attendees raw_attendee[]);
+
+SET check_function_bodies = OFF;
+
+CREATE OR REPLACE VIEW "public"."event_stats" AS
+SELECT
+    e.id,
+    e.created_at,
+    e.account_id,
+    e.cal_id,
+    e.series,
+    e.at,
+    event_length (e.at) AS length,
+    e.title,
+    (e.is_meeting
+        AND (count(*) > 1)
+        AND (count(*) FILTER (WHERE ((a.response = 'accepted'::attendance)
+            AND (e.account_id = a.account_id))) = 1)) AS is_meeting,
+e.is_online,
+e.is_onsite,
+e.is_offsite,
+count(*) FILTER (WHERE (a.response = 'accepted'::attendance)) AS attendee_count,
+string_agg((a.account_id)::text, ','::text ORDER BY a.account_id) AS attendees,
+count(*) AS invitee_count
+FROM (event e
+    JOIN attendee a ON (e.id = a.event_id))
+GROUP BY
+    e.id;
+

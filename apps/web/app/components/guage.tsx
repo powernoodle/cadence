510ce3a8f7dfed942d91242f5b6697c3a856a8e0
Kris Braun <kris.braun@letscollide.io>
2023-06-27 18:30:13 -0400

    Target editing controls

--- apps/web/app/components/guage.tsx
+++ apps/web/app/components/guage.tsx
@@ -1,5 +1,8 @@
+import { useState } from "react";
 import {
   AspectRatio,
+  Button,
+  NumberInput,
   Box,
   Center,
   Text,
@@ -10,7 +13,12 @@ import {
   useMantineColorScheme,
 } from "@mantine/core";
 import { ResponsivePie } from "@nivo/pie";
-import { IconArrowUpRight, IconArrowDownRight } from "@tabler/icons-react";
+import {
+  IconArrowUpRight,
+  IconArrowDownRight,
+  IconCheck,
+  IconX,
+} from "@tabler/icons-react";
 
 import { makeColor } from "../color";
 import { durationFmt } from "../util";
@@ -76,7 +84,7 @@ export function Guage({
             enableArcLinkLabels={false}
             tooltip={(d) =>
               d.datum.label ? (
-                <Paper p={2}>
+                <Paper p={2} pl="xs" pr="xs" bg="dark.7">
                   <Text color={d.datum.color}>{d.datum.label}</Text>
                 </Paper>
               ) : null
@@ -184,91 +192,132 @@ export function TargetGuage({
   maximize?: boolean;
 }) {
   const { colorScheme } = useMantineColorScheme();
-  if (targetMinutes === undefined) {
-    return (
-      <Guage
-        sections={[]}
-        label={
-          <Text
-            fz={24}
-            color={makeColor("gray", 6, 5, colorScheme)}
-            ta="center"
-          >
-            Set target
-          </Text>
-        }
-        total={projectedMinutes * 2}
-      />
-    );
-  }
-  let sections;
-  if (projectedMinutes < targetMinutes) {
-    sections = [
-      {
-        label: "Projected",
-        value: projectedMinutes,
-        color: "green",
-        shade: 4,
-      },
-      {
-        label: "Under target",
-        value: targetMinutes - projectedMinutes,
-        color: maximize ? "red" : "green",
-        shade: maximize ? 4 : 2,
-      },
-    ];
-  } else {
-    sections = [
-      {
-        label: "Target",
-        value: targetMinutes,
-        color: "green",
-        shade: 4,
-      },
-      {
-        label: "Over target",
-        value: projectedMinutes - targetMinutes,
-        color: maximize ? "green" : "red",
-        shade: maximize ? 2 : 4,
-      },
-    ];
+  const [edit, setEdit] = useState(false);
+  let sections: GuageSections[] = [];
+  if (targetMinutes) {
+    if (projectedMinutes < targetMinutes) {
+      sections = [
+        {
+          label: "Projected",
+          value: projectedMinutes,
+          color: "green",
+          shade: 4,
+        },
+        {
+          label: "Under target",
+          value: targetMinutes - projectedMinutes,
+          color: maximize ? "red" : "green",
+          shade: maximize ? 4 : 2,
+        },
+      ];
+    } else {
+      sections = [
+        {
+          label: "Target",
+          value: targetMinutes,
+          color: "green",
+          shade: 4,
+        },
+        {
+          label: "Over target",
+          value: projectedMinutes - targetMinutes,
+          color: maximize ? "green" : "red",
+          shade: maximize ? 2 : 4,
+        },
+      ];
+    }
   }
   const DiffIcon = maximize ? IconArrowUpRight : IconArrowDownRight;
   return (
     <Guage
       sections={sections}
-      total={targetMinutes * 2}
+      total={targetMinutes ? targetMinutes * 2 : 0}
       label={
-        <Stack spacing={0}>
-          <Text
-            fz="lg"
-            color={makeColor("gray", 6, 5, colorScheme)}
-            ta="center"
-            mt="md"
-          >
-            Target
-          </Text>
-          <Text
-            fz={32}
-            fw={700}
-            color={makeColor("gray", 8, 2, colorScheme)}
-            ta="center"
-          >
-            {durationFmt(targetMinutes)}
-          </Text>
-          <Text
-            fz="sm"
-            color={makeColor("gray", 6, 5, colorScheme)}
-            ta="center"
-          >
-            hours/week
-          </Text>
-          <Text fz="sm" color={makeColor("gray", 6, 5, colorScheme)} mt="md">
-            <Group spacing={0}>
-              <DiffIcon size="1rem" stroke={1.5} />
-              <span>&nbsp;is better</span>
+        <Stack spacing={0} align="center">
+          {(targetMinutes !== undefined || edit) && (
+            <Text fz="lg" color={makeColor("gray", 6, 5, colorScheme)} mt="md">
+              Target
+            </Text>
+          )}
+          {!edit && (
+            <Button
+              variant="subtle"
+              onClick={() => setEdit(true)}
+              sx={{ height: "50px" }}
+            >
+              {targetMinutes === undefined && (
+                <Text
+                  fz={24}
+                  fw={300}
+                  color={makeColor("gray", 6, 5, colorScheme)}
+                >
+                  Set target
+                </Text>
+              )}
+              {targetMinutes !== undefined && (
+                <Text
+                  fz={32}
+                  fw={700}
+                  color={makeColor("gray", 8, 2, colorScheme)}
+                >
+                  {durationFmt(targetMinutes)}
+                </Text>
+              )}
+            </Button>
+          )}
+          {edit && (
+            <Group pt={4} pb={4} spacing={2} sx={{ height: "50px" }}>
+              <NumberInput
+                defaultValue={3600}
+                precision={0}
+                min={0}
+                max={3600}
+                sx={{ width: "5em" }}
+              />
+              <Text>:</Text>
+              <NumberInput
+                defaultValue={0}
+                formatter={(n) => String(n).padStart(2, "0")}
+                precision={0}
+                min={0}
+                max={59}
+                sx={{ width: "4em" }}
+              />
             </Group>
-          </Text>
+          )}
+          {(targetMinutes !== undefined || edit) && (
+            <Text fz="sm" color={makeColor("gray", 6, 5, colorScheme)}>
+              hours/week
+            </Text>
+          )}
+          {targetMinutes !== undefined && !edit && (
+            <Text fz="sm" color={makeColor("gray", 6, 5, colorScheme)} mt="md">
+              <Group spacing={0}>
+                <DiffIcon size="1rem" stroke={1.5} />
+                <span>&nbsp;is better</span>
+              </Group>
+            </Text>
+          )}
+          {edit && (
+            <Group mt="xs" spacing={0}>
+              <Button
+                compact
+                variant="subtle"
+                onClick={() => setEdit(false)}
+                title="Save"
+              >
+                <IconCheck />
+              </Button>
+              <Button
+                compact
+                variant="subtle"
+                onClick={() => setEdit(false)}
+                title="Cancel"
+              >
+                <IconX />
+              </Button>
+            </Group>
+          )}
         </Stack>
       }
     />
